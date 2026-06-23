519bd9b61f3853a68e3b0bfca0abe077702c334b
LeBonyt <lebonyt@gmail.com>
2026-06-23 13:51:15 +0200

    Projektseite

--- www/public/pvoc.py
+++ www/public/pvoc.py
@@ -1,11 +1,28 @@
 import webview
 import os
+import threading
+import http.server
+import socketserver
 
-html_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'projekte.html')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PORT = 18432
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=BASE_DIR, **kwargs)
+    def log_message(self, format, *args):
+        pass  # kein Output im Terminal
+
+def start_server():
+    with socketserver.TCPServer(('127.0.0.1', PORT), Handler) as httpd:
+        httpd.serve_forever()
+
+thread = threading.Thread(target=start_server, daemon=True)
+thread.start()
 
 webview.create_window(
     'PVOC – Projektübersicht',
-    f'file://{html_file}',
+    f'http://127.0.0.1:{PORT}/projekte.html',
     width=1200,
     height=800,
     resizable=True
